d9425362470e44ea0cc0079fd8d3b866d813ffb1
dhanushkomari <dhanushkomari@gmail.com>
2021-10-05 14:15:09 +0530

    made some changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'mobile', 'department', 'designation', 'employee_id')
+        fields = ('first_name', 'last_name', 'email', 'mobile', 'department', 'designation', 'employee_id', 'employee_leaves')
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
